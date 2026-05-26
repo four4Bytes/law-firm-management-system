ce4f9d287469312e32fe31c33f08069f88bbd132
r4ppz <johnreyrabosa.f@gmail.com>
2026-05-26 22:35:50 +0800

    fix: add favicon icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "@/styles/variables.css";
 export const metadata: Metadata = {
   title: "Management System",
   description: "Capstone project for a private law firm ;P",
+  icons: "/favicon.png",
 };
 
 export default function RootLayout({

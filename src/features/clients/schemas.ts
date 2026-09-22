0860b290149c414c0185f5051c9a5d4f2969d08b
r4ppz <johnreyrabosa.f@gmail.com>
2026-09-22 13:00:33 +0800

    chore(client): cleanup dead codes

--- src/features/clients/schemas.ts
+++ src/features/clients/schemas.ts
@@ -1,17 +1,5 @@
 import { z } from "zod";
 
-import { ClientDataSchema } from "@/lib/schemas";
-
-export const ClientCreatePayloadSchema = ClientDataSchema;
-
-export type ClientCreatePayload = z.infer<typeof ClientCreatePayloadSchema>;
-
 export const ClientIdSchema = z.object({
   clientId: z.uuid(),
 });
-
-export const ClientUpdatePayloadSchema = ClientDataSchema.extend({
-  clientId: z.uuid(),
-});
-
-export type ClientUpdatePayload = z.infer<typeof ClientUpdatePayloadSchema>;
